f5e57a9140b2150407ebae02e2439b300f682b81
Evgeniitimofeev <129780530+Evgeniitimofeev@users.noreply.github.com>
2023-04-10 20:53:11 +0300

    Update final_win.py

--- final_win.py
+++ final_win.py
@@ -1,1 +1,17 @@
-# напиши здесь код третьего экрана приложения
+from PyQt5.QtCore import Qt
+from PyQt5.QtCore import (QApplication, QWidget, QHBoxLayout, QVBoxLayout, QGridLayout, QGroupBox, QRadioButton,  QPushButton, QLabel, QListWidget, QLineEdit)
+
+from instr import *
+
+class FinalWin( QWidget)
+  def __init__(self):
+    super().__init__()
+    self.initUT()
+    self.set_appear()
+    self.show()
+  def initUT():
+    self.workh_text = QLabel(txt_workheart)
+    self.index_text = QLabel(txt_index)
+    self.layout_line = QVBoxLayout()
+    self.layout_line.addWidet(self.index_text, alignment = Qt.AlignCenter)
+    self.layout_line.addWidet(self.workh_text
